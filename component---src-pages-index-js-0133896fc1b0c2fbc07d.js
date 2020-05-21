550d84ad90d879fe91898bcc5f53b31f6f962e29
labrose <labrose@github.com>
2020-05-21 16:50:45 -0700

    Updates

---
+++ component---src-pages-index-js-0133896fc1b0c2fbc07d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{RXBc:function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),c=t.n(o);e.default=function(){return c.a.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-0133896fc1b0c2fbc07d.js.map
