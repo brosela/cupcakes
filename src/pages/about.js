65318e167a671dc36632bdfba1a91cc69a1fd327
labrose <labrose@github.com>
2020-05-21 16:57:05 -0700

    Added about page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>About me!</h1>
+      <p>I'm a person who loves cupcakes</p>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default function Home() {
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+    </div>
+  );
+}
